d00eda6885d38fec7b184c86a22af7c43b37982c
gabriel alves <gabreel.contato@gmail.com>
2025-12-06 23:18:25 -0300

    feat: add method delete, get and login to token

---
+++ src/main/java/com/gabriel/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.gabriel.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+
+    public ResourceNotFoundException(String msg, Throwable throwable){
+        super(msg, throwable);
+    }
+}
